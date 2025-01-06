628aad2faf9342b9d7cc8ece24e88c7c27a381b4
RAKESHM28102000 <mrakeshmrakesh885@gmail.com>
2025-01-06 20:27:04 +0530

    updated image

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => (
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
-      <img src="../image/rakeshaiimage__1_-removebg-preview.png" alt="profile_bg" />
+      <img src="../image/New Project.png" alt="profile_bg" />
       <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
